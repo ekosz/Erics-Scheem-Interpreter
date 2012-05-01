e4dbbd5f89db2ce12f18ab3cb1d4e2255907c3eb
Eric Koslow <ekoslow@gmail.com>
2012-05-01 15:44:48 -0400

    Methods raise errors when passed the wrong number of arguments

--- eval.js
+++ eval.js
@@ -1,3 +1,9 @@
+var argumentSize = function (function_name, need, got) {
+    if (need !== got) {
+        throw('Wrong number of arguements for '+function_name+
+              '. Required: '+need+'. Recived: '+got);
+    }
+}
 var evalScheem = function (expr, env) {
     // Numbers evaluate to themselves
     if (typeof expr === 'number') {
@@ -20,63 +26,80 @@ var evalScheem = function (expr, env) {
             return evalScheem(expr[i], env);
         // Quote
         case 'quote':
+            argumentSize('quote', 1, expr.slice(1).length);
             return expr[1];
         // List manipulators
         case 'cons':
+            argumentSize('cons', 2, expr.slice(1).length);
             return [evalScheem(expr[1], env)].concat(
                     evalScheem(expr[2], env));
         case 'car':
+            argumentSize('car', 1, expr.slice(1).length);
             return evalScheem(expr[1], env)[0];
         case 'cdr':
+            argumentSize('cdr', 1, expr.slice(1).length);
             return evalScheem(expr[1], env).slice(1);
         // Set variables
         case 'define':
+            argumentSize('define', 2, expr.slice(1).length);
             env[expr[1]] = evalScheem(expr[2], env);
             return 0;
         case 'set!':
+            argumentSize('set!', 2, expr.slice(1).length);
             env[expr[1]] = evalScheem(expr[2], env);
             return 0;
         // Math operations
         case '+':
+            argumentSize('+', 2, expr.slice(1).length);
             return evalScheem(expr[1], env) + evalScheem(expr[2], env);
         case '-':
+            argumentSize('-', 2, expr.slice(1).length);
             return evalScheem(expr[1], env) - evalScheem(expr[2], env);
         case '*':
+            argumentSize('*', 2, expr.slice(1).length);
             return evalScheem(expr[1], env) * evalScheem(expr[2], env);
         case '/':
+            argumentSize('/', 2, expr.slice(1).length);
             return evalScheem(expr[1], env) / evalScheem(expr[2], env);
         // Compairors
         case '=':
+            argumentSize('=', 2, expr.slice(1).length);
             var eq =
                 (evalScheem(expr[1], env) ===
                  evalScheem(expr[2], env));
             if (eq) return '#t';
             return '#f';
         case "<":
+            argumentSize('<', 2, expr.slice(1).length);
             var lt =
                 (evalScheem(expr[1], env) <
                  evalScheem(expr[2], env));
             if (lt) return '#t';
             return '#f';
         case "<=":
+            argumentSize('<=', 2, expr.slice(1).length);
             var lte =
                 (evalScheem(expr[1], env) <=
                  evalScheem(expr[2], env));
             if (lte) return '#t';
             return '#f';
         case ">":
+            argumentSize('>', 2, expr.slice(1).length);
             var gt =
                 (evalScheem(expr[1], env) >
                  evalScheem(expr[2], env));
             if (gt) return '#t';
             return '#f';
         case ">=":
+            argumentSize('>=', 2, expr.slice(1).length);
             var gt =
                 (evalScheem(expr[1], env) >=
                  evalScheem(expr[2], env));
             if (gte) return '#t';
             return '#f';
+        // Simple if statement
         case 'if':
+            argumentSize('if', 3, expr.slice(1).length);
             if(evalScheem(expr[1], env) === '#t') {
                 return evalScheem(expr[2], env);
             } else {
